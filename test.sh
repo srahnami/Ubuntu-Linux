5c879c41f9e8de1dafec74f7e7b2dcc39bbe63b0
Sarah Tsui <109244249+srahnami@users.noreply.github.com>
2022-07-15 15:46:56 -0400

    Update test.sh
    added a description of the task

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
+# deletes existing folder in dir where this file is executed(to clear any with matching name), 
+# then creates 2 new directories, with 1 subdirectory in one of the directories.
+# check contents list for dir
+# deletes both directories
 rm -r srahtest
 mkdir srahtest -p hi/hello
 ls
